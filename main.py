c0b62c2c3289126d6e9a473eca0b06e10a83faad
Matheus Mendes <matheushenriquesad@gmail.com>
2024-01-12 18:19:11 -0300

    Ajustes para multiplos devices

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from functions import main
 
 START = 0 
-PLUS_START = 8
-TIME_TO_ADD = 5
+PLUS_START = 100
+TIME_TO_ADD = 30
 GROUP_NAME = 'Grupe'
-PEOPLE_TO_ADD = 8
+PEOPLE_TO_ADD = 100
 
 main(chat_name=GROUP_NAME, start=START, number_of_people_to_add=PEOPLE_TO_ADD, time_to_finish=TIME_TO_ADD)
